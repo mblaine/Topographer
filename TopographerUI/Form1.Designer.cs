f9e404bdc7eaf9fadbc6695e22b1bd5b6320d70d
Matthew Blaine <mblaine@ymail.com>
2012-04-15 09:45:13 -0400

    Fixed controls overlapping under a normal dpi.

--- TopographerUI/Form1.Designer.cs
+++ TopographerUI/Form1.Designer.cs
@@ -37,8 +37,8 @@
             this.btnRender = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.spnLimitHeight = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
+            this.spnLimitHeight = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.spnLimitHeight)).BeginInit();
@@ -49,15 +49,17 @@
             this.txtWorldPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtWorldPath.Location = new System.Drawing.Point(12, 12);
+            this.txtWorldPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtWorldPath.Name = "txtWorldPath";
             this.txtWorldPath.ReadOnly = true;
-            this.txtWorldPath.Size = new System.Drawing.Size(447, 22);
+            this.txtWorldPath.Size = new System.Drawing.Size(503, 22);
             this.txtWorldPath.TabIndex = 0;
             // 
             // btnOpenWorld
             // 
             this.btnOpenWorld.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOpenWorld.Location = new System.Drawing.Point(465, 11);
+            this.btnOpenWorld.Location = new System.Drawing.Point(521, 11);
+            this.btnOpenWorld.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOpenWorld.Name = "btnOpenWorld";
             this.btnOpenWorld.Size = new System.Drawing.Size(108, 23);
             this.btnOpenWorld.TabIndex = 1;
@@ -70,8 +72,10 @@
             this.groupBox1.Controls.Add(this.radEnd);
             this.groupBox1.Controls.Add(this.radNether);
             this.groupBox1.Controls.Add(this.radOverworld);
-            this.groupBox1.Location = new System.Drawing.Point(12, 40);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Size = new System.Drawing.Size(245, 53);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
@@ -81,8 +85,9 @@
             // 
             this.radEnd.AutoSize = true;
             this.radEnd.Location = new System.Drawing.Point(184, 22);
+            this.radEnd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radEnd.Name = "radEnd";
-            this.radEnd.Size = new System.Drawing.Size(54, 21);
+            this.radEnd.Size = new System.Drawing.Size(72, 26);
             this.radEnd.TabIndex = 2;
             this.radEnd.TabStop = true;
             this.radEnd.Text = "End";
@@ -92,9 +97,10 @@
             // radNether
             // 
             this.radNether.AutoSize = true;
-            this.radNether.Location = new System.Drawing.Point(106, 22);
+            this.radNether.Location = new System.Drawing.Point(107, 22);
+            this.radNether.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radNether.Name = "radNether";
-            this.radNether.Size = new System.Drawing.Size(72, 21);
+            this.radNether.Size = new System.Drawing.Size(96, 26);
             this.radNether.TabIndex = 1;
             this.radNether.TabStop = true;
             this.radNether.Text = "Nether";
@@ -106,8 +112,9 @@
             this.radOverworld.AutoSize = true;
             this.radOverworld.Checked = true;
             this.radOverworld.Location = new System.Drawing.Point(7, 22);
+            this.radOverworld.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radOverworld.Name = "radOverworld";
-            this.radOverworld.Size = new System.Drawing.Size(93, 21);
+            this.radOverworld.Size = new System.Drawing.Size(124, 26);
             this.radOverworld.TabIndex = 0;
             this.radOverworld.TabStop = true;
             this.radOverworld.Text = "Overworld";
@@ -118,7 +125,8 @@
             // 
             this.btnRender.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnRender.Enabled = false;
-            this.btnRender.Location = new System.Drawing.Point(465, 40);
+            this.btnRender.Location = new System.Drawing.Point(521, 39);
+            this.btnRender.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRender.Name = "btnRender";
             this.btnRender.Size = new System.Drawing.Size(108, 23);
             this.btnRender.TabIndex = 3;
@@ -129,9 +137,9 @@
             // lblStatus
             // 
             this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblStatus.Location = new System.Drawing.Point(263, 70);
+            this.lblStatus.Location = new System.Drawing.Point(319, 70);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(310, 23);
+            this.lblStatus.Size = new System.Drawing.Size(309, 23);
             this.lblStatus.TabIndex = 4;
             this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
@@ -139,23 +147,35 @@
             // 
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.spnLimitHeight);
-            this.groupBox2.Location = new System.Drawing.Point(263, 40);
+            this.groupBox2.Location = new System.Drawing.Point(263, 39);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(155, 53);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(165, 53);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Options";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Limit Height:";
+            // 
             // spnLimitHeight
             // 
-            this.spnLimitHeight.Location = new System.Drawing.Point(98, 22);
+            this.spnLimitHeight.Location = new System.Drawing.Point(97, 23);
+            this.spnLimitHeight.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.spnLimitHeight.Maximum = new decimal(new int[] {
             255,
             0,
             0,
             0});
             this.spnLimitHeight.Name = "spnLimitHeight";
-            this.spnLimitHeight.Size = new System.Drawing.Size(45, 22);
+            this.spnLimitHeight.Size = new System.Drawing.Size(56, 22);
             this.spnLimitHeight.TabIndex = 0;
             this.spnLimitHeight.Value = new decimal(new int[] {
             255,
@@ -163,20 +183,11 @@
             0,
             0});
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Limit Height:";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 103);
+            this.ClientSize = new System.Drawing.Size(641, 103);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnRender);
@@ -184,6 +195,7 @@
             this.Controls.Add(this.btnOpenWorld);
             this.Controls.Add(this.txtWorldPath);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
